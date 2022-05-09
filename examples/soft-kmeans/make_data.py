0f03feeb98da507d8e4c1a2d681354e2632cc866
Simeon Carstens <simeon.carstens@tweag.io>
2022-05-09 11:13:13 +0200

    Clean up / format Python code

--- examples/soft-kmeans/make_data.py
+++ examples/soft-kmeans/make_data.py
@@ -1,21 +1,22 @@
 from typing import List, Tuple
 import numpy as np
 
+
 def generate_data(
-        means: List[Tuple[float]],
-        n_points: List[int],
-    ):
+    means: List[Tuple[float]],
+    n_points: List[int],
+):
     """
     Generates clusterized data points.
-    
+
     Args:
         means: List of the coordinates of the means of the generating gaussians. The length of the tuples should be consistent and determines the dimension.
         n_points: Number of point for each cluster. The length should be equal to the length of `means`.
-    
+
     Returns:
         A 2D numpy array of shape (n_data_points, n_dim) containing the data points.
     """
-    np.random.seed(1)   
+    np.random.seed(1)
     means = np.array(means, dtype=np.float64)
     n_dim = means.shape[1]
     cov = np.eye(N=n_dim, dtype=np.float64)
@@ -26,7 +27,7 @@ def generate_data(
         data_points += list(data)
     data_points = np.array(data_points).squeeze()
     # Save data
-    filename = "data.txt"
+    filename = "data_2D.txt"
     print(f"Saving data points in `{filename}`")
     np.savetxt(filename, data_points)
     # Return
--- examples/soft-kmeans/probability.py
+++ examples/soft-kmeans/probability.py
@@ -15,49 +15,45 @@ class Pdf:
         prior_mean,
         weights,
         data,
-        likelihood_sigma = 1,
+        likelihood_sigma=1,
     ):
         self.prior_sigma = prior_sigma
         self.prior_mean = prior_mean
-        self.weights = np.array(weights)/np.array(weights).sum()
+        self.weights = np.array(weights) / np.array(weights).sum()
         self.data = data
         self.likelihood_sigma = likelihood_sigma
         self.n_dim = data.shape[1]
+        self.n_clusters = len(self.weights)
 
     def log_prior(self, x):
-        # x is a sample, or a point in the space of the posterior,
-        # which is of dimension n_clusters*n_dim
-        # Reshape x to dimensions (n_clusters, n_dim)
-        x = x.reshape(-1, self.n_dim)
+        x = x.reshape(self.n_clusters, self.n_dim)
         return -0.5 * np.sum((x - self.prior_mean) ** 2, 1).sum() \
             / self.prior_sigma ** 2
 
     def log_prior_gradient(self, x):
-        # Reshape x to dimensions (n_clusters, n_dim)
-        x = x.reshape(-1, self.n_dim)
+        x = x.reshape(self.n_clusters, self.n_dim)
         bla = -(x - self.prior_mean) / self.prior_sigma ** 2
         return bla.ravel()
 
     def log_likelihood(self, x):
         # Reshape x & data to dimensions (n_clusters, n_dim, n_data_points)
-        x = x.reshape(-1, self.n_dim)[:,:,np.newaxis] 
-        data = self.data.T[np.newaxis,:,:]
+        x = x.reshape(self.n_clusters, self.n_dim)[:, :, np.newaxis]
+        data = self.data.T[np.newaxis, :, :]
         a = logsumexp(
-            -0.5 * np.sum((x - data) ** 2, 1) 
-            + np.log(self.weights[:,np.newaxis])
-            / (self.likelihood_sigma ** 2)
-        , 0)
+            -0.5 * np.sum((x - data) ** 2, 1)
+            + np.log(self.weights[:, np.newaxis])
+            / (self.likelihood_sigma ** 2), 0)
         return a.sum()
 
     def log_likelihood_gradient(self, x):
         # Reshape x & data to dimensions (n_clusters, n_dim, n_data_points)
-        x = x.reshape(-1, self.n_dim)[:,:,np.newaxis]
-        data = self.data.T[np.newaxis,:,:]
+        x = x.reshape(self.n_clusters, self.n_dim)[:, :, np.newaxis]
+        data = self.data.T[np.newaxis, :, :]
         d_outer = softmax(
             -0.5 * np.sum((x - data) ** 2, 2)
-            / (self.likelihood_sigma ** 2) 
-            + np.log(self.weights)[:,np.newaxis])
-        im = -(x - data) * d_outer[:,:,np.newaxis]
+            / (self.likelihood_sigma ** 2)
+            + np.log(self.weights)[:, np.newaxis])
+        im = -(x - data) * d_outer[:, :, np.newaxis]
         return im.sum(axis=2).ravel()
 
     def log_prob(self, x):
@@ -66,7 +62,7 @@ class Pdf:
     def log_prob_gradient(self, x):
         return self.log_prior_gradient(x) + self.log_likelihood_gradient(x)
 
-# n_dim = 1
+
 pdf = Pdf(
     prior_sigma=5,
     prior_mean=np.array([[1], [1]]),
@@ -75,7 +71,9 @@ pdf = Pdf(
 )
 initial_states = np.array([1., 0.])
 
-# # n_dim = 2
+
+# 2D example
+
 # pdf = Pdf(
 #     prior_sigma=5,
 #     prior_mean=np.array([[0, 0], [0, 0]]),
