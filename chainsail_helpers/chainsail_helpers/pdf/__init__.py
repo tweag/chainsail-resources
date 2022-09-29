fc2f27a37b8d7e8d07794c0e22041ee419dac607
Simeon Carstens <simeon.carstens@tweag.io>
2022-09-29 20:57:22 +0200

    Add posterior PDF interface to chainsail-helpers

--- chainsail_helpers/chainsail_helpers/pdf/__init__.py
+++ chainsail_helpers/chainsail_helpers/pdf/__init__.py
@@ -5,7 +5,7 @@ Abstract PDF interfaces for Chainsail
 
 class PDF:
     """
-    Minimal interface all probability densities consumed by Chainsail have
+    Minimal interface general probability densities consumed by Chainsail have
     to conform to.
     """
 
@@ -33,5 +33,65 @@ class PDF:
         Returns:
             np.ndarray: 1D array of floats containing the flattened
               log-probability gradient evaluated at x
+        """
+        pass
+
+
+class PosteriorPDF:
+    """
+    Interface for posterior probability densities consumed by Chainsail
+    """
+
+    def log_likelihood(self, x):
+        """
+        Log-likelihood function of the model.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the log-likelihood
+              function is evaluated
+
+        Returns:
+            float: log-likelihood function evaluated at x
+        """
+        pass
+
+    def log_likelihood_gradient(self, x):
+        """
+        Gradient of the log-likelihood function of the model.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the gradient of the
+              log-likelihood function is evaluated
+
+        Returns:
+            np.ndarray: 1D array of floats containing the flattened
+              log-likelihood function gradient evaluated at x
+        """
+        pass
+
+    def log_prior(self, x):
+        """
+        Log-prior of the model.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the log-prior
+              is evaluated
+
+        Returns:
+            float: log-prior evaluated at x
+        """
+        pass
+
+    def log_prior_gradient(self, x):
+        """
+        Gradient of the log-prior of the model.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the gradient of the
+              log-prior is evaluated
+
+        Returns:
+            np.ndarray: 1D array of floats containing the flattened
+              log-prior gradient evaluated at x
         """
         pass
