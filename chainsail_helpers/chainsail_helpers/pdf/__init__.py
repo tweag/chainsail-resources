531752eb15fbfeb595ce5b934e4d548a9c8d33f3
Simeon Carstens <simeon.carstens@tweag.io>
2022-10-18 18:19:01 +0200

    Make PDF abstract and have PosteriorPDF inherit from it

--- chainsail_helpers/chainsail_helpers/pdf/__init__.py
+++ chainsail_helpers/chainsail_helpers/pdf/__init__.py
@@ -1,14 +1,16 @@
 """
 Abstract PDF interfaces for Chainsail
 """
+from abc import abstractmethod, ABCMeta
 
 
-class PDF:
+class PDF(ABCMeta):
     """
     Minimal interface general probability densities consumed by Chainsail have
     to conform to.
     """
 
+    @abstractmethod
     def log_prob(self, x):
         """
         Log-probability of the density to be sampled.
@@ -22,6 +24,7 @@ class PDF:
         """
         pass
 
+    @abstractmethod
     def log_prob_gradient(self, x):
         """
         Gradient of the log-probability of the density to be sampled.
@@ -37,11 +40,11 @@ class PDF:
         pass
 
 
-class PosteriorPDF:
+class PosteriorPDF(PDF):
     """
     Interface for posterior probability densities consumed by Chainsail
     """
-
+    @abstractmethod
     def log_likelihood(self, x):
         """
         Log-likelihood function of the model.
@@ -55,6 +58,7 @@ class PosteriorPDF:
         """
         pass
 
+    @abstractmethod
     def log_likelihood_gradient(self, x):
         """
         Gradient of the log-likelihood function of the model.
@@ -69,6 +73,7 @@ class PosteriorPDF:
         """
         pass
 
+    @abstractmethod
     def log_prior(self, x):
         """
         Log-prior of the model.
@@ -82,6 +87,7 @@ class PosteriorPDF:
         """
         pass
 
+    @abstractmethod
     def log_prior_gradient(self, x):
         """
         Gradient of the log-prior of the model.
@@ -95,3 +101,30 @@ class PosteriorPDF:
               log-prior gradient evaluated at x
         """
         pass
+
+    def log_prob(self, x):
+        """
+        Log-posterior density to be sampled.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the log-posterior
+              is evaluated
+
+        Returns:
+            float: log-posterior evaluated at x
+        """
+        return self.log_likelihood(x) + self.log_prior(x)
+
+    def log_prob_gradient(self, x):
+        """
+        Gradient of the log-posterior of the density to be sampled.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the log-posterior
+              gradient is evaluated
+
+        Returns:
+            np.ndarray: 1D array of floats containing the flattened
+              log-posterior gradient evaluated at x
+        """
+        return self.log_likelihood_gradient(x) + self.log_prior_gradient(x)
