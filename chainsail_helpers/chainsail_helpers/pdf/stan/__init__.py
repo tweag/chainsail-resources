804a81466caaa80409494ff4bcc43f8ebbb97ba0
Simeon Carstens <simeon.carstens@tweag.io>
2022-12-18 18:01:30 +0100

    Add support for likelihood tempering to Stan PDF interface

--- chainsail_helpers/chainsail_helpers/pdf/stan/__init__.py
+++ chainsail_helpers/chainsail_helpers/pdf/stan/__init__.py
@@ -42,6 +42,74 @@ class StanPDF(PDF):
         self._httpstan_model_route = f"{self._HTTPSTAN_URL}/v1/{model_id}"
         self._data = data or {}
 
+    @staticmethod
+    def _tag_data(data, include_prior, include_likelihood):
+        return {**data,
+                "include_prior": int(include_prior),
+                "include_likelihood": int(include_likelihood)}
+
+    def _query_log_prob(self, x, include_prior=True, include_likelihood=True):
+        try:
+            r = requests.post(
+                f"{self._httpstan_model_route}/log_prob",
+                json={
+                    "unconstrained_parameters": x.tolist(),
+                    "data": self._tag_data(self._data, include_prior, include_likelihood),
+                    "adjust_transform": False,
+                },
+            )
+            r.raise_for_status()
+            return r.json()["log_prob"]
+        except Exception as e:
+            raise Exception(f"Querying log-prob failed: Error: {e}")
+
+    def _query_log_prob_gradient(self, x, include_prior=True, include_likelihood=True):
+        try:
+            r = requests.post(
+                f"{self._httpstan_model_route}/log_prob_grad",
+                json={
+                    "unconstrained_parameters": x.tolist(),
+                    "data": self._tag_data(self._data, include_prior, include_likelihood),
+                    "adjust_transform": False,
+                },
+            )
+            r.raise_for_status()
+            return np.array(r.json()["log_prob_grad"])
+        except Exception as e:
+            raise Exception(f"Querying log-prob gradient failed: Error: {e}")
+
+    def log_likelihood(self, x):
+        """
+        Evaluates the log-likelihood of the model.
+        
+        Calls out to the httpstan server specified in self._httpstan_url with
+        datums `include_prior` set to `0` and `include_likelihood` set to `1`.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the log-likelihood
+              is evaluated
+
+        Returns:
+            float: log-likelihood evaluated at x
+        """
+        return self._query_log_prob(x, include_prior=False)
+
+    def log_prior(self, x):
+        """
+        Evaluates the log-prior probability of the model.
+        
+        Calls out to the httpstan server specified in self._httpstan_url with
+        datums `include_prior` set to `1` and `include_likelihood` set to `0`.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the log-prior probability
+              is evaluated
+
+        Returns:
+            float: log-prior probability evaluated at x
+        """
+        return self._query_log_prob(x, include_likelihood=False)
+    
     def log_prob(self, x):
         """
         Log-probability of the density to be sampled.
@@ -53,20 +121,40 @@ class StanPDF(PDF):
         Returns:
             float: log-probability evaluated at x
         """
-        try:
-            r = requests.post(
-                f"{self._httpstan_model_route}/log_prob",
-                json={
-                    "unconstrained_parameters": x.tolist(),
-                    "data": self._data,
-                    "adjust_transform": False,
-                },
-            )
-            r.raise_for_status()
-            return r.json()["log_prob"]
-        except Exception as e:
-            raise Exception(f"Querying log-prob failed: Error: {e}")
+        return self._query_log_prob(x)
 
+    def log_likelihood_gradient(self, x):
+        """
+        Evaluates the gradient of the log-likelihood of the model.
+        Calls out to the httpstan server specified in self._httpstan_url
+        with datums `include_prior` set to `0` and `include_likelihood` set to `1`.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the gradient of the
+              log-likelihood is evaluated
+
+        Returns:
+            np.ndarray: 1D array of floats containing the flattened
+              gradient of the log-likelihood evaluated at x
+        """
+        return self._query_log_prob_gradient(x, include_prior=False)
+
+    def log_prior_gradient(self, x):
+        """
+        Evaluates the gradient of the log-prior density of the model.
+        Calls out to the httpstan server specified in self._httpstan_url
+        with datums `include_prior` set to `1` and `include_likelihood` set to `0`.
+
+        Args:
+            x(np.ndarray): 1D array of floats at which the gradient of the
+              log-prior density is evaluated
+
+        Returns:
+            np.ndarray: 1D array of floats containing the flattened
+              gradient of the log-prior density evaluated at x
+        """
+        return self._query_log_prob_gradient(x, include_likelihood=False)
+        
     def log_prob_gradient(self, x):
         """
         Gradient of the log-probability of the density to be sampled.
@@ -80,16 +168,4 @@ class StanPDF(PDF):
             np.ndarray: 1D array of floats containing the flattened
               log-probability gradient evaluated at x
         """
-        try:
-            r = requests.post(
-                f"{self._httpstan_model_route}/log_prob_grad",
-                json={
-                    "unconstrained_parameters": x.tolist(),
-                    "data": self._data,
-                    "adjust_transform": False,
-                },
-            )
-            r.raise_for_status()
-            return np.array(r.json()["log_prob_grad"])
-        except Exception as e:
-            raise Exception(f"Querying log-prob gradient failed: Error: {e}")
+        return self._query_log_prob_gradient(x)
