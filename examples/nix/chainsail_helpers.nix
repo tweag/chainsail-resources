439fb52be52774db47bef409cd9b6313477ac568
Simeon Carstens <simeon.carstens@tweag.io>
2023-04-24 14:26:55 +0200

    Update hash

--- examples/nix/chainsail_helpers.nix
+++ examples/nix/chainsail_helpers.nix
@@ -4,7 +4,7 @@ buildPythonPackage rec {
   version = "0.1.5";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "TODO after PyPi publication";
+    sha256 = "3f01e4e98fe4b13e675a92b7c2e44ce4df1b3fccad058e73ed140c5a5d85ca35";
   };
   doCheck = false;
   propagatedBuildInputs = [ numpy ];
