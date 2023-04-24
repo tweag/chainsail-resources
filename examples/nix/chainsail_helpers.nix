0dde7c555bc96732ab7a41bc9758573e0ce0e313
Simeon Carstens <simeon.carstens@tweag.io>
2023-04-24 12:11:53 +0200

    Remove Python version cap

--- examples/nix/chainsail_helpers.nix
+++ examples/nix/chainsail_helpers.nix
@@ -1,7 +1,7 @@
 { buildPythonPackage, fetchPypi, numpy }:
 buildPythonPackage rec {
   pname = "chainsail-helpers";
-  version = "0.1.5";
+  version = "0.1.5.1";
   src = fetchPypi {
     inherit pname version;
     sha256 = "3f01e4e98fe4b13e675a92b7c2e44ce4df1b3fccad058e73ed140c5a5d85ca35";
